3924e40bdf099b078f4ac667ac1bd0dbd7af7bac
BuildTools <unconfigured@null.spigotmc.org>
2024-06-05 21:31:56 +0200

    Ajout d'une multiplication implicite pour corriger integrale_analytique

--- calculs.py
+++ calculs.py
@@ -12,5 +12,5 @@ def evaluation(P, value):
     return P[0]+P[1]*value+P[2]*value**2+P[3]*value**3
 
 def integrale_analytique(P, a, b):
-    integrale = (P[0]b+P[1]*(b**2)/2+P[2]*(b**3)/3+P[3]*(b**4)/4)-(P[0]a+P[1]*(a**2)/2+P[2]*(a**3)/3+P[3]*(a**4)/4)
+    integrale = (P[0]*b+P[1]*(b**2)/2+P[2]*(b**3)/3+P[3]*(b**4)/4)-(P[0]*a+P[1]*(a**2)/2+P[2]*(a**3)/3+P[3]*(a**4)/4)
     return integrale
