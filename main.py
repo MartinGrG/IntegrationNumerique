a6055924909807cc7739ab93a5b472aa269a0576
BuildTools <unconfigured@null.spigotmc.org>
2024-06-05 23:25:30 +0200

    modifications mineures

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from methode_simpson import *
 
 resultat_comp = compare(methode_trapezes_python, methode_trapezes_numpy, [1,2,3,4], 0 , 10)
 
-print(resultat_comp[0][:,0])
-print(resultat_comp[1][:,1])
+print(resultat_comp[0,:,:])
+print("\n")
+print(resultat_comp[1,:,:])
 
