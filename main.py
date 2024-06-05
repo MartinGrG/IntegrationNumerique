626c8dbd3911c4edc7ceba7f4a1eae48d975f5b7
BuildTools <unconfigured@null.spigotmc.org>
2024-06-05 23:00:31 +0200

    Amélioration de l'affichage des résultats de compare

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ from methode_simpson import *
 
 
 
-print(compare(methode_trapezes_python, methode_trapezes_numpy, [1,2,3,4], 0 , 10))
+resultat_comp = compare(methode_trapezes_python, methode_trapezes_numpy, [1,2,3,4], 0 , 10)
+
+print(resultat_comp[0][:,0])
+print(resultat_comp[1][:,1])
+
