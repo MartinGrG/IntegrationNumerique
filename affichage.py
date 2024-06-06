dc196705ec95063f18afd6c3da928328a120797c
amark <alexismarkiewicz020@gmail.com>
2024-06-05 22:20:27 -0400

    Ajout des tracés des temps de réponses et des erreurs des fonctions

--- affichage.py
+++ affichage.py
@@ -1,19 +1,42 @@
 import matplotlib.pyplot as plt
-import analyse
-import methode_rectangles
-import methode_trapezes
-import methode_simpson
+from analyse import *
+from methode_rectangles import *
+from methode_trapezes import *
+from methode_simpson import *
 
-# Tracé temps de réponse de chaque méthode
+
 a = 0
 b = 10
-liste_nbre_seg = [i*(b-a) for i in range(1,11)]
-rect_base_tps = compare(methode_rectangle_python, a, b)
-rect_numpy_tps = compare(methode_rectangle_numpy, a, b)
-trap_base_tps = compare(methode_trapeze_python, a, b)
-trap_numpy_tps = compare(methode_trapeze_numpy, a, b)
-simp_base_tps = compare(methode_simpson_python, a, b)
-simp_numpy_tps = compare(methode_simpson_numpy, a, b)
+coef = [1, 2, 3, 4]
+
+
+def recuperer_tps(liste_compare):
+    """Cette fonction permet d'extraire les temps de réponse du renvoi de la fonction compare
+    pour les 2 fonctions comparées"""
+    liste_tps_fct1 = []
+    liste_tps_fct2 = []
+    for i in range(1,len(liste_compare[0])):
+        liste_tps_fct1.append(liste_compare[0][i][0])
+        liste_tps_fct2.append(liste_compare[1][i][0])
+    return liste_tps_fct1, liste_tps_fct2
+
+
+def recuperer_err(liste_compare):
+    """Cette fonction permet d'extraire les erreurs du renvoi de la fonction compare
+        pour les 2 fonctions comparées"""
+    liste_err_fct1 = []
+    liste_err_fct2 = []
+    for i in range(1, len(liste_compare[0])):
+        liste_err_fct1.append(liste_compare[0][i][2])
+        liste_err_fct2.append(liste_compare[1][i][2])
+    return liste_err_fct1, liste_err_fct2
+
+
+# Tracé temps de réponse de chaque méthode
+liste_nbre_seg = [i*(b-a) for i in range(1,12)]
+rect_base_tps, rect_numpy_tps = recuperer_tps(compare(methode_rectangle_python, methode_rectangle_numpy, coef, a, b))
+trap_base_tps, trap_numpy_tps = recuperer_tps(compare(methode_trapezes_python, methode_trapezes_numpy, coef, a, b))
+simp_base_tps, simp_numpy_tps = recuperer_tps(compare(methode_simpson_python, methode_simpson_numpy, coef, a, b))
 
 plt.plot(liste_nbre_seg, rect_base_tps, 'r-', label='Méthode des rectangles de base')
 plt.plot(liste_nbre_seg, rect_numpy_tps, 'r--', label='Méthode des rectangles Numpy')
@@ -30,19 +53,13 @@ plt.legend()
 plt.show()
 
 
-# Tracé erreur de chaque méthode
-a = 0
-b = 10
-liste_nbre_seg = [i*(b-a) for i in range(1,11)]
-rect_base_err = compare(methode_rectangle_python, a, b)
-rect_numpy_err = compare(methode_rectangle_numpy, a, b)
-trap_base_err = compare(methode_trapeze_python, a, b)
-trap_numpy_err = compare(methode_trapeze_numpy, a, b)
-simp_base_err = compare(methode_simpson_python, a, b)
-simp_numpy_err = compare(methode_simpson_numpy, a, b)
+# Tracé de l'erreur de chaque méthode
+rect_base_err, rect_numpy_err = recuperer_err(compare(methode_rectangle_python, methode_rectangle_numpy, coef, a, b))
+trap_base_err, trap_numpy_err = recuperer_err(compare(methode_trapezes_python, methode_trapezes_numpy, coef, a, b))
+simp_base_err, simp_numpy_err = recuperer_err(compare(methode_simpson_python, methode_simpson_numpy, coef, a, b))
 
 plt.plot(liste_nbre_seg, rect_base_err, 'r-', label='Méthode des rectangles de base')
-plt.plot(liste_nbre_seg, rect_nump_err, 'r--', label='Méthode des rectangles Numpy')
+plt.plot(liste_nbre_seg, rect_numpy_err, 'r--', label='Méthode des rectangles Numpy')
 plt.plot(liste_nbre_seg, trap_base_err, 'b-', label='Méthode des trapèzes de base')
 plt.plot(liste_nbre_seg, trap_numpy_err, 'b--', label='Méthode des trapèzes Numpy')
 plt.plot(liste_nbre_seg, simp_base_err, 'g-', label='Méthode Simpson de base')
--- main.py
+++ main.py
@@ -7,6 +7,5 @@ from methode_simpson import *
 
 resultat_comp = compare(methode_trapezes_python, methode_trapezes_numpy, [1,2,3,4], 0 , 10)
 
-print(resultat_comp[0][:,0])
-print(resultat_comp[1][:,1])
-
+print(len(resultat_comp))
+print(resultat_comp[0][1][0])
