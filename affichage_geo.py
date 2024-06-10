07c7090701b3b1ce20e94a35e2790ca75a9b7cab
Martin Grangé <martin.grange.1@ens.etsmtl.ca>
2024-06-10 16:45:57 -0400

    Amérlioration affichage geo

--- affichage_geo.py
+++ affichage_geo.py
@@ -46,6 +46,10 @@ def affichage_rectangle(liste, a, b, n):  # Affichage de la méthode des rectang
     plt.plot(x_rectangle, y_rectangle2, color='orange')
     plt.fill_between(x_rectangle, y_rectangle2, 0, color='orange', alpha=0.3)
 
+    x_segment = x_rectangle[2::]
+    y_segment = y_rectangle2[2::]
+    plt.vlines(x_segment, 0, y_segment, colors='orange', linewidth=1)
+
     # Plot fonction analytique
     x = np.linspace(a, b, 1000)
     y_analytique = evaluation(liste, x)
@@ -65,6 +69,9 @@ def affichage_trapeze(liste, a, b, n):
 
     plt.plot(x_trapeze, y_trapeze, color='orange')
     plt.fill_between(x_trapeze, y_trapeze, 0, color='orange', alpha=0.3)
+    plt.vlines(x_trapeze, 0, y_trapeze, colors='orange', linewidth=1)
+
+    plt.scatter(x_trapeze, y_trapeze, color='red', s=15)
 
     # Plot fonction analytique
     x_analytique = np.linspace(a, b, 1000)
@@ -85,6 +92,9 @@ def affichage_simpson(liste, a, b, n):
         print("Le nombre de sous-intervalles n doit être pair.")
         return 12
 
+    x_simpson = []
+    y_simpson = []
+
     x = np.linspace(a, b, n + 1)
     y = evaluation(liste, x)
 
@@ -93,17 +103,27 @@ def affichage_simpson(liste, a, b, n):
     y_analytique = evaluation(liste, x_analytique)
 
     # Tracer la courbe analytique
-    plt.plot(x_analytique, y_analytique, label=f'f(x) = {liste[0]} + {liste[1]}x + {liste[2]}x^2 + {liste[3]}x^3', color='blue')
+    plt.plot(x_analytique, y_analytique, label=f'f(x) = {liste[0]} + {liste[1]}x + {liste[2]}x^2 + {liste[3]}x^3', color= 'blue')
 
     # Tracer les segments parabolique de Simpson et colorier l'aire
     for i in range(0, n, 2):
-        x_simpson = np.linspace(x[i], x[i + 2], 100)
-        y_simpson = np.polyval(np.polyfit([x[i], x[i + 1], x[i + 2]], [y[i], y[i + 1], y[i + 2]], 2), x_simpson)
-        plt.plot(x_simpson, y_simpson, color='orange')
-        plt.fill_between(x_simpson, 0, y_simpson, color='orange', alpha=0.3)
+        x_simpson2 = np.linspace(x[i], x[i + 2], 10)
+        y_simpson2 = np.polyval(np.polyfit([x[i], x[i + 1], x[i + 2]], [y[i], y[i + 1], y[i + 2]], 2), x_simpson2)
+        x_simpson.append(x_simpson2)
+        y_simpson.append(y_simpson2)
+
+    x_simpson = np.concatenate(x_simpson)
+    y_simpson = np.concatenate(y_simpson)
+
+    plt.plot(x_simpson, y_simpson, color='orange')
+    plt.fill_between(x_simpson, 0, y_simpson, color='orange', alpha=0.3, label='Approximation de l\'aire' if i == 0 else "")
 
     # Ajouter des points
-    plt.scatter(x, y, color='red')
+    plt.scatter(x, y, color='red', s=15)
+
+    x_segment = x[::2]
+    y_segment = y[::2]
+    plt.vlines(x_segment, 0, y_segment, colors='orange', linewidth=1)
 
     # Configurer le graphique
     plt.xlabel('x')
@@ -115,4 +135,4 @@ def affichage_simpson(liste, a, b, n):
     return 0
 
 
-affichage_trapeze([1,2,3,4],0,10,4)
+affichage_rectangle([1, -10, 20, -4], 0, 10, 8)
