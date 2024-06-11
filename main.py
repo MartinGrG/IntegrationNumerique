f8a5ad902244ffd4dfabcdfa2f9d18ac577c03c6
BuildTools <unconfigured@null.spigotmc.org>
2024-06-11 21:54:14 +0200

    Amélioration de la lisibilité du plot

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import main_Martin
-import affichage3d
-import affichage
+# Pour lancer les affichages de la géométrie des méthodes :
+#           - Lancer main_Martin
+
+# Pour lancer les affichages comparatifs entre les méthodes :
+#           - Lancer affichage
+
+# Pour lancer les affichages 3D qui compare nombre de segments,
+# temps de calcul et erreur relative :
+#           - Lancer affichage_3d
