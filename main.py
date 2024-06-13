d980d812b4b302178e334b3683fc18363c2ca230
MartinGrG <martin.grange.1@ens.etsmtl.ca>
2024-06-12 22:23:24 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# Pour lancer les affichages de la géométrie des méthodes :
-#           - Lancer main_Martin
-
-# Pour lancer les affichages comparatifs entre les méthodes :
-#           - Lancer affichage
-
-# Pour lancer les affichages 3D qui compare nombre de segments,
-# temps de calcul et erreur relative :
-#           - Lancer affichage_3d
